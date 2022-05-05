0a3da0ee955f1123f633cd7a25a82d9cc7b8b9b7
Sitdikov DR <sdr-u@mail.ru>
2022-05-05 11:46:34 +0500

    created a simple emitter - Emitter.ts

---
+++ src/utils/Emitter.test.ts
@@ -0,0 +1,37 @@
+import Emitter from './Emitter'
+
+class TestArgument { }
+
+describe('Emitter', () => {
+    test('test', () => {
+        const emitter = new Emitter<TestArgument>()
+
+        const fn1 = jest.fn<void, [TestArgument]>()
+        emitter.add(fn1)
+        expect(fn1).toBeCalledTimes(0)
+
+        const fn2 = jest.fn<void, [TestArgument]>()
+        emitter.add(fn2)
+        expect(fn1).toBeCalledTimes(0)
+        expect(fn2).toBeCalledTimes(0)
+
+        const argument = new TestArgument()
+        emitter.emit(argument)
+        expect(fn1).toBeCalledTimes(1)
+        expect(fn1).toBeCalledWith<[TestArgument]>(argument)
+        expect(fn2).toBeCalledTimes(1)
+        expect(fn2).toBeCalledWith<[TestArgument]>(argument)
+
+        emitter.remove(fn1)
+        expect(fn1).toBeCalledTimes(1)
+        expect(fn1).toBeCalledWith<[TestArgument]>(argument)
+        expect(fn2).toBeCalledTimes(1)
+        expect(fn2).toBeCalledWith<[TestArgument]>(argument)
+
+        const argument2 = new TestArgument()
+        emitter.emit(argument2)
+        expect(fn1).toBeCalledTimes(1)
+        expect(fn2).toBeCalledTimes(2)
+        expect(fn2).toBeCalledWith<[TestArgument]>(argument2)
+    })
+})
