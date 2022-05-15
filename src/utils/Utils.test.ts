6ad69442422e8758bcb42ba99b4c6609a689f533
Sitdikov DR <sdr-u@mail.ru>
2022-05-15 10:16:05 +0500

    created an "uuid" generator

---
+++ src/utils/Utils.test.ts
@@ -0,0 +1,15 @@
+import Utils from './Utils'
+
+describe('Utils', () => {
+    test('uuid', () => {
+        let repeats = 10
+        let prevUuid: string | undefined
+        while (repeats > 0) {
+            repeats--
+            const currentUuid = Utils.uuid()
+            expect(currentUuid).toHaveLength(36)
+            expect(currentUuid != prevUuid).toBeTruthy()
+            prevUuid = currentUuid
+        }
+    })
+})
