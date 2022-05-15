7699874707d32591fd36a7e0ae74e2b634e21108
Sitdikov DR <sdr-u@mail.ru>
2022-05-15 10:47:08 +0500

    - created api-entity
    - created api-word-mock

---
+++ src/api/api-entity.ts
@@ -0,0 +1,15 @@
+import { Unsubscribe } from '../utils/common-types'
+
+export type ChangeData<T> = { type: 'added' | 'edited' | 'removed', data: T }
+export type OnChangesFn<T> = (changes: ChangeData<T>[] | Error) => void
+
+export interface IApiEntity<T, TPayload> {
+    changesTracking(onChanges: OnChangesFn<T>): Unsubscribe
+
+    get(id: string): Promise<T | null>
+    getList(): Promise<T[]>
+
+    create(payload: TPayload): Promise<T>
+    edit(id: string, payload: TPayload): Promise<void>
+    remove(id: string): Promise<void>
+}
