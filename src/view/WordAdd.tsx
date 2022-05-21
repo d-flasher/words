696e54309fa892e2cb69d0452c4be6699344336b
Sitdikov DR <sdr-u@mail.ru>
2022-05-21 09:31:29 +0500

    handling create/edit bugs

--- src/view/WordAdd.tsx
+++ src/view/WordAdd.tsx
@@ -3,16 +3,22 @@ import { FC, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { IWordPayload } from '../model/word'
+import Utils from '../utils/utils'
 import WordForm from './WordForm'
-import { ApiContext } from './WordsApp'
+import { ApiContext, ControllerContext } from './WordsApp'
 
 const WordAdd: FC = () => {
     const navigate = useNavigate()
     const api = useContext(ApiContext)
+    const { serviceMessages } = useContext(ControllerContext)
 
     const onSave = async (payload: IWordPayload) => {
-        await api.words.create(payload)
-        navigate('/words')
+        try {
+            await api.words.create(payload)
+            navigate('/words')
+        } catch (error) {
+            serviceMessages.add(Utils.asError(error).message, 'error')
+        }
     }
 
     const onCancel = () => {
