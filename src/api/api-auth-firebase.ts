18f27c6aab85729b742f1b0466a05517fc621f4e
Sitdikov DR <sdr-u@mail.ru>
2022-05-23 10:26:09 +0500

    a refactoring

--- src/api/api-auth-firebase.ts
+++ src/api/api-auth-firebase.ts
@@ -1,18 +1,25 @@
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, User } from 'firebase/auth'
+import { Auth, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, User } from 'firebase/auth'
 
 import IApiAuth from './api-auth'
 
 class ApiAuthFirebase implements IApiAuth {
+
+    private _auth: Auth
+
+    constructor() {
+        this._auth = getAuth()
+    }
+
     signIn(email: string, password: string) {
-        return signInWithEmailAndPassword(getAuth(), email, password)
+        return signInWithEmailAndPassword(this._auth, email, password)
     }
 
     signOut() {
-        return signOut(getAuth())
+        return signOut(this._auth)
     }
 
     onAuthStateChanged(handler: (user: User | null) => void) {
-        return onAuthStateChanged(getAuth(), handler)
+        return onAuthStateChanged(this._auth, handler)
     }
 }
 export default ApiAuthFirebase
